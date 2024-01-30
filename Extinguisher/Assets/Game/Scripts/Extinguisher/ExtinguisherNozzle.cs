ee29f1ef691d2cd80c42c6172b38b9e31c1d5250
jacniluk <jacniluk@wp.pl>
2024-01-30 20:32:08 +0100

    Nozzle animation

--- Extinguisher/Assets/Game/Scripts/Extinguisher/ExtinguisherNozzle.cs
+++ Extinguisher/Assets/Game/Scripts/Extinguisher/ExtinguisherNozzle.cs
@@ -1,8 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
 public class ExtinguisherNozzle : ExtinguisherElement, IClickable
 {
+    [Header("References")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private AnimationClip actionAnimation;
+
     public void OnClickDown() // IClickable
     {
+        StartCoroutine(ActionCoroutine());
+    }
+
+    private IEnumerator ActionCoroutine()
+    {
         interactionTrigger.enabled = false;
+
+        animator.SetTrigger("Action");
+
+        yield return new WaitForSeconds(actionAnimation.length);
+
+        animator.enabled = false;
 
         extinguisherController.Action();
     }

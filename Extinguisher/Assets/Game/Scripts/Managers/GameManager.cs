bac420c6cdd656a4c26caa8745d8f8e889a24e8d
jacniluk <jacniluk@wp.pl>
2024-01-31 20:35:21 +0100

    Restart and exit game in Game Manager

--- Extinguisher/Assets/Game/Scripts/Managers/GameManager.cs
+++ Extinguisher/Assets/Game/Scripts/Managers/GameManager.cs
@@ -1,9 +1,25 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     private void Awake()
     {
         Application.targetFrameRate = 60;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }

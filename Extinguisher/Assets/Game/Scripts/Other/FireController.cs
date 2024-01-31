0416f18b834ceb74c18d45a8bbab5eb85765a915
jacniluk <jacniluk@wp.pl>
2024-01-31 10:00:02 +0100

    Fire

---
+++ Extinguisher/Assets/Game/Scripts/Other/FireController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    [Header("Data")]
+    [SerializeField] private float firePower;
+
+    [Header("References")]
+    [SerializeField] private ParticleSystem fireEffect;
+
+    private float startRateOverTime;
+    private float currentFirePower;
+    private bool isBeingExtinguished;
+
+    private void Awake()
+    {
+        startRateOverTime = fireEffect.emission.rateOverTime.constant;
+        currentFirePower = firePower;
+    }
+
+    private void Update()
+    {
+        UpdateFire();
+    }
+
+    private void UpdateFire()
+    {
+        if (isBeingExtinguished)
+        {
+            currentFirePower -= Time.deltaTime;
+            if (currentFirePower < 0.0f)
+            {
+                Destroy(gameObject);
+            }
+
+            SetRateOverTime();
+        }
+        else if (currentFirePower < firePower)
+        {
+            currentFirePower += Time.deltaTime;
+            if (currentFirePower > firePower)
+            {
+                currentFirePower = firePower;
+            }
+
+            SetRateOverTime();
+        }
+    }
+
+    private void SetRateOverTime()
+    {
+        ParticleSystem.EmissionModule emissionModule = fireEffect.emission;
+        emissionModule.rateOverTime = Utilities.Evaluate(currentFirePower, 0.0f, firePower, 0.0f, startRateOverTime);
+    }
+
+    public void StartExtinguish()
+    {
+        isBeingExtinguished = true;
+    }
+
+    public void StopExtinguish()
+    {
+        isBeingExtinguished = false;
+    }
+}
--- Extinguisher/Assets/Game/Scripts/Utilities/Utilities.cs
+++ Extinguisher/Assets/Game/Scripts/Utilities/Utilities.cs
@@ -8,4 +8,34 @@ public class Utilities : MonoBehaviour
         return layerMask == (layerMask | (1 << layer));
     }
     #endregion
+
+    #region Progress
+    public static float Evaluate(float value, float rangeMin, float rangeMax, float resultMin, float resultMax)
+    {
+        float progress = CalculateProgress01(value, rangeMin, rangeMax);
+        float result = Evaluate(progress, resultMin, resultMax);
+
+        return result;
+    }
+
+    private static float CalculateProgress01(float value, float min, float max)
+    {
+        float achieved = value - min;
+        float toAchieve = max - min;
+        float progress = achieved / toAchieve;
+        progress = Mathf.Clamp01(progress);
+
+        return progress;
+    }
+
+    private static float Evaluate(float progress, float resultMin, float resultMax)
+    {
+        float result = resultMin;
+        float gain = resultMax - resultMin;
+        float profit = progress * gain;
+        result += profit;
+
+        return result;
+    }
+    #endregion
 }
